7b18bd215242283111cf3f6ec1d3ee36d0f20618
Janek <janekputz@web.de>
2019-02-07 20:28:55 +0100

    add basic functionality (table with time diff)

--- MMM-Event-Counter.js
+++ MMM-Event-Counter.js
@@ -1,14 +1,48 @@
 'use strict';
 
-Module.register("Event Counter", {
+Module.register("MMM-Event-Counter", {
+
    defaults: {
        text: "Hello world!"
    } ,
 
     // Override dom generator
     getDom: function () {
+
+        var config = this.config;
         var wrapper = document.createElement("div");
-        wrapper.innerHTML = this.config.text;
+        var table = document.createElement("table");
+
+        for (var i = 0; i < config.events.length; i++) {
+
+            var event = config.events[i];
+
+            // create fields
+            var eventTitle = document.createElement("td");
+            $(eventTitle).text(event.name);
+
+            var eventTime = document.createElement("td");
+            var timeDiff = Math.abs(new Date(event.date).getTime() - new Date().getTime());
+            var diffDays = Math.ceil(timeDiff / (1000 * 3600 * 24));
+            $(eventTime).text(diffDays + " Tage");
+
+            // append as row
+            var eventRow = document.createElement("tr");
+            $(eventRow).append(eventTitle);
+            $(eventRow).append(eventTime);
+            $(table).append(eventRow);
+
+        }
+
+        $(wrapper).append(table);
         return wrapper;
+
+    },
+
+    getScripts: function() {
+        return [
+            'https://code.jquery.com/jquery-2.2.3.min.js',  // this file will be loaded from the jquery servers.
+        ]
     }
+
 });
--- README.md
+++ README.md
@@ -1,3 +1,51 @@
 # MMM-Event-Counter
 
 This is a module for the [MagicMirror²](https://github.com/MichMich/MagicMirror/) project which can displays the remaining time to a number of events.
+
+## Installation
+
+Navigate into your MagicMirror's modules folder:
+
+``$ cd ~/MagicMirror/modules``
+
+Clone this repository:
+
+``$ git clone https://github.com/Janek7/MMM-Event-Counter.git``
+
+Configure the module in your config.js file.
+
+## Using module
+Add the following JavaScript object to your modules array in ``config/config.js``
+````
+{
+    module: 'MMM-Event-Counter',
+    position: 'top_left',
+    header: 'Events',
+    config: {
+        events: [
+            {
+                name: 'Holidays',
+                date: '2019-06-08'
+            },
+            {
+                name: 'Exam Period',
+                date: '2019-07-01'
+            },
+            ...
+        ],
+    },
+}
+````
+
+## Configuration
+Every event can be configured through the following options
+
+| Option | Description | Required |
+|----------------- |----------- | ------- |
+| `name` | name of the upcoming event | ✅  |
+| `date` | date of the upcoming event to count down in format `YYYY-MM-DD` | ✅  |
+| `icon` | displayed icon | ❌  |
+
+
+## Dependencies
+This module uses [jQuery](https://jquery.com/).
